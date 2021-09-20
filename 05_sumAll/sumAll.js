d26a23979f106088dfb2e4b556fa35df3a45c103
Daniel <mtz.daniel21@gmail.com>
2021-09-19 21:49:03 -0500

    finished exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,14 +1,10 @@
 const sumAll = function (num1, num2) {
   // Check for negatives and non numbers
-  if (num1 < 0 || num2 < 0) {
+  if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+    return "ERROR";
+  } else if (num1 < 0 || num2 < 0) {
     return "ERROR";
   }
-  if (isNaN(num1) || isNaN(num2)) {
-    return "ERROR";
-  } else if (!(Number.isInteger(num1) || Number.isInteger(num2))) {
-    return "ERROR";
-  }
-
   // switch num1 and num2 if num2 if num 1 is greater
   if (num1 > num2) {
     temp = num1;
