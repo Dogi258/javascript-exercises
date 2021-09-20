e60a9bdcd793cf207d073009cbafef8273342534
Daniel <mtz.daniel21@gmail.com>
2021-09-19 22:38:11 -0500

    finished exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,25 @@
-const ftoc = function() {
+const ftoc = function (temp) {
+  let tempConversion = (temp - 32) * (5 / 9);
 
+  // Check to see if temperature is a whole number
+  if (tempConversion % 1 != 0) {
+    return parseFloat(tempConversion.toFixed(1));
+  } else {
+    return tempConversion;
+  }
 };
 
-const ctof = function() {
-
+const ctof = function (temp) {
+  let tempConversion = temp * (9 / 5) + 32;
+  if (tempConversion % 1 != 0) {
+    return parseFloat(tempConversion.toFixed(1));
+  } else {
+    return tempConversion;
+  }
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
